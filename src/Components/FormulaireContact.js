e1459e1838e430f44f62f6ad6e4894c936468e72
Ozbroome <eric.rodriguez.pp@gmail.com>
2017-08-29 22:01:27 +0200

    modifications css

---
+++ src/Components/FormulaireContact.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class FormulaireContact extends Component {
+
+
+
+    render() {
+
+
+        return (
+            <div>
+                <h1>Contact</h1>
+            </div>
+        );
+    }
+}
+
+export default FormulaireContact;
